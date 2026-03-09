0d6ac88f6661202bc3ef21a0c450b3181deac072
vuongthanhtuan <18521609@gm.uit.edu.vn>
2026-03-10 00:10:56 +0700

    Chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,31 @@ import taskRoute from './routes/tasksRouters.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}));
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin: "http://localhost:5173"}));
+}
+
 app.use("/api/tasks", taskRoute);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+};
+
 connectDB().then(() => {
     app.listen(PORT, () =>{
         console.log(`Server bat dau tren cong ${PORT}`);
